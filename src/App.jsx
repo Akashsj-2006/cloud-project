ced8a7af3820b1428126a54e7e2cc2ff6c129ea9
Akash SJ <144226046+Akashsj-2006@users.noreply.github.com>
2025-10-16 20:27:15 +0530

    Fix import paths for Home and Coin components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {Routes, Route} from "react-router-dom"
-import Home from "./Pages/Home/Home"
-import Coin from "./Pages/Coin/Coin"
+import Home from "./pages/Home/Home"
+import Coin from "./pages/Coin/Coin"
 import Footer from './Components/Footer/Footer'
 
 
